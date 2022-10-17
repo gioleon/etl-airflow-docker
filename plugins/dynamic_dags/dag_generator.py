a63ea32b47817b9e03e9154afcfbd31e424c2f8b
gioleon <giovannileon2017@gmail.com>
2022-10-17 10:50:45 -0500

    changes in the scripts interaction

--- plugins/dynamic_dags/dag_generator.py
+++ plugins/dynamic_dags/dag_generator.py
@@ -12,6 +12,17 @@ parent_dir = Path(__file__).parent.parent.parent
 # Enviroment
 env = Environment(loader=FileSystemLoader(file_dir))
 
+# IF not exists dags dir, we will create it.
+if not os.path.exists(Path(
+        parent_dir, 'dags'
+    )):
+    os.makedirs(
+        Path(
+            parent_dir,
+            'dags'
+    )
+)
+
 # Template
 template = env.get_template('DD_template.jinja2')
 
